561d172405abddb36ee6d662ad2ded262868bc7b
kchs0529 <kchs0529@gmail.com>
2023-05-09 20:48:15 +0900

    function1,3,5

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm* time1, * time2;
 
 void filestat1();
 void filestat2();
@@ -16,57 +16,57 @@ void blockcmp();
 void datecmp();
 void timecmp();
 
-int main(){
-    filestat1();
-    filestat2();
-    filetime1();
-    filetime2();
-    sizecmp();
-    blockcmp();
-    datecmp();
-    timecmp();
+int main() {
+	filestat1();
+	filestat2();
+	filetime1();
+	filetime2();
+	sizecmp();
+	blockcmp();
+	datecmp();
+	timecmp();
 }
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1() //2018202054 김찬희
 {
-    stat("text1", &stat1);
+	stat("text1", &stat1);
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2() //2018202054 김찬희
 {
-    stat("text2", &stat2);
+	stat("text2", &stat2);
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
-void filetime1(){
-    
+void filetime1() {
+
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
-void filetime2(){
-    
+void filetime2() {
+
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp() //2018202054 김찬희
 {
-    printf("size compare\n");
-    if (stat1.st_size > stat2.st_size) {
-        printf("text 1 is bigger\n\n");
-    }
-    else if (stat1.st_size < stat2.st_size) {
-        printf("text 2 is bigger\n\n");
-    }
-    else {
-        printf("sizes are equal\n\n");
-    }
+	printf("size compare\n");
+	if (stat1.st_size > stat2.st_size) {
+		printf("text 1 is bigger\n\n");
+	}
+	else if (stat1.st_size < stat2.st_size) {
+		printf("text 2 is bigger\n\n");
+	}
+	else {
+		printf("sizes are equal\n\n");
+	}
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
-void blockcmp(){
-    
+void blockcmp() {
+
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
@@ -112,6 +112,6 @@ void datecmp(void)//2018202054 김찬희
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
-void timecmp(){
-    
+void timecmp() {
+
 }
